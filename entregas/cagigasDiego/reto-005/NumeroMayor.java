39b3a0616cd1d31faaed14e67ca162a52b2786ea
diegocagigas <145450815+diegocagigas@users.noreply.github.com>
2023-10-11 13:51:18 +0200

    Create NumeroMayor.java

---
+++ entregas/cagigasDiego/reto-005/NumeroMayor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Numeromayor {
+public static void main (String[] args){
+Scanner sc = new scanner (system.in);
+
+System.out.println("Ingresa el primer numero: ");
+int num1= sc.NextInt();
+
+System.out.println("Ingresa el segundo numero: ");
+int num2= sc.NextInt();
+
+System.out.println("El numero mayor es: ");
+System.out.println (num1 > num2 ? num1 : num2);
+}
+}
